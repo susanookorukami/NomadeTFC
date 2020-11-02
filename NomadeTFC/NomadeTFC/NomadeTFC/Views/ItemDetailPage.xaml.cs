d0707f51166177763df3bcbc1fcab98d4b6af152
tedi kpognon <tedi.kpognon@epsi.fr>
2020-11-02 08:52:11 +0100

    start

---
+++ NomadeTFC/NomadeTFC/NomadeTFC/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using NomadeTFC.ViewModels;
+
+namespace NomadeTFC.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
