e1f6a061b0d1352d55f62b03160c184349a7f96d
tedi kpognon <tedi.kpognon@epsi.fr>
2020-11-04 10:57:24 +0100

    feuture complite but not test

---
+++ NomadeTFC/NomadeTFCTest/NewVilleViewModelTest.cs
@@ -0,0 +1,26 @@
+﻿using NomadeTFC.ViewModels;
+using NUnit.Framework;
+using NomadeTFC.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NomadeTFCTest
+{
+    public class NVVMtest
+    {
+        NewVilleViewModel Nvvm;
+
+        [SetUp]
+        public void Setup()
+        {
+            Nvvm = new NewVilleViewModel();
+        }
+
+        [Test]
+        public void NewVilleViewModelTest()
+        {
+            Nvvm.Nom = " ";
+        }
+    }
+}
