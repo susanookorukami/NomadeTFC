e2afc23b78160f5e905af81f40ddf53f9c1b54d6
Oulimata77 <oulimatacoly4@gmail.com>
2020-11-04 10:53:43 +0100

    Modiif

---
+++ NomadeTFC/NomadeTFCTest/VilleViewModelTest.cs
@@ -0,0 +1,24 @@
+﻿using NomadeTFC.ViewModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NomadeTFCTest
+{
+   public class Test
+    {
+        VilleViewModel _testV;
+
+        [SetUp]
+        public void Setup()
+        {
+            _testV = new VilleViewModel();
+        }
+        [Test]
+        public void VilleVie
+        {
+
+        }
+    }
+}
