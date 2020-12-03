60d67b53b6e14e48b1c1f6390d350c601c8d0d16
epsib1-ff <florent.fourvel@epsi.fr>
2020-12-03 10:46:34 +0100

    modification AboutPage.xaml et AboutViewModel.cs

--- NomadeTFC/NomadeTFC/NomadeTFC/ViewModels/AboutViewModel.cs
+++ NomadeTFC/NomadeTFC/NomadeTFC/ViewModels/AboutViewModel.cs
@@ -10,7 +10,7 @@ namespace NomadeTFC.ViewModels
         public AboutViewModel()
         {
             Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamain-quickstart"));
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://openweathermap.org/weathermap?basemap=map&cities=true&layer=temperature&lat=30&lon=-20&zoom=5"));
         }
 
         public ICommand OpenWebCommand { get; }
